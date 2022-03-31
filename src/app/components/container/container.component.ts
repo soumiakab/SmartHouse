04e394defd575b3ce87ca9df3ba001802291fbba
KABRANESOUMIA <kabrane.soumia.com>
2022-03-31 14:22:36 +0000

    add change status

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { LampService } from '../../services/lamp/lamp.service';
+import { TvService } from '../../services/tv/tv.service';
+import { CameraService } from '../../services/camera/camera.service';
 import { DeviceService } from '../../services/device/device.service';
 import {StatusEnum} from 'src/app/core/enums/status-enum';
 import { DeviceList } from 'src/app/core/models/deviceList';
-
+import { Lamp } from 'src/app/core/models/lamp';
+import { Tv } from 'src/app/core/models/tv';
+import { Camera } from 'src/app/core/models/camera';
+import { DeviceEnum } from 'src/app/core/enums/deviceEnum';
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
@@ -12,16 +18,24 @@ import { DeviceList } from 'src/app/core/models/deviceList';
 export class ContainerComponent implements OnInit {
 
   devices:DeviceList ={};
-  constructor(private deviceService: DeviceService){
+  lamps:Lamp[] = [];
+  tvs:Tv[] =[];
+  cameras:Camera[] = [];
+
+   type: DeviceEnum[] = [DeviceEnum.Lamp,DeviceEnum.CAMERA,DeviceEnum.TV];
+
+
+  constructor(private deviceService: DeviceService, private lampService: LampService,private tvService: TvService,private cameraService: CameraService){
   }
 
   async ngOnInit():Promise<void>{
     await this.getDevices();
-    console.log(this.devices);
   }
 
    async getDevices(){
-    this.deviceService.getDevices().subscribe(devices =>{console.log(devices);this.devices = devices});
+    this.lampService.getLamps().subscribe(lamps =>{console.log(lamps);this.lamps = lamps});
+    this.tvService.getTvs().subscribe(tvs =>{console.log(tvs);this.tvs = tvs});
+    this.cameraService.getCameras().subscribe(cameras =>{console.log(cameras);this.cameras = cameras});
 
   }
 
@@ -41,17 +55,21 @@ export class ContainerComponent implements OnInit {
 
   title = 'smartHouseProject';
 
-  changeStatus(status:StatusEnum){
-    if(status==StatusEnum.ON) {
-      this.iconClass=this.iconOffClass;
-      this.containerClass=this.offClass;
-      this.butttonClass=this.offButton;
+  changeStatus(object:any,type:DeviceEnum){
+    object.status=this.turn(object.status);
+    switch(type){
+      case DeviceEnum.Lamp:
+        this.lampService.updateLamp(object as Lamp);
+        break;
+      case DeviceEnum.TV:
+        this.tvService.updateTv(object as Tv);
+        break;
+      case DeviceEnum.CAMERA:
+        this.cameraService.updateCamera(object as Camera);
+        break;
     }
-    else{
-      this.iconClass=this.iconOnClass;
-      this.containerClass=this.onClass;
-      this.butttonClass=this.onButton;
-    }
+    //this.getDevices();
+
   }
   iconName(status:StatusEnum){
     if(status==StatusEnum.ON) {
@@ -62,4 +80,12 @@ export class ContainerComponent implements OnInit {
     }
 }
 
+  turn(status:StatusEnum){
+    if(status==StatusEnum.ON) {
+      return StatusEnum.OFF;
+    }
+    else{
+      return StatusEnum.ON;
+    }
+  }
 }
